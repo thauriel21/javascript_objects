bfbba7a1037237457af08ca5418cce33c410e542
Alynson Roldan <alynsonrolfu@gmai.com>
2022-10-26 12:33:38 -0300

    ejercicios de pokemon

--- 03_instance/main.js
+++ 03_instance/main.js
@@ -1,27 +1,34 @@
-function Pokemon(name, height, hp, defense, aP) {
+function Pokemon(name, height, hp, defense, aP , id) {
     this.name = name,
         this.height = height
-    this.hp = this.hp
+    this.hp = hp
     this.defense = defense
     this.attackPoints = aP
-
+    this.id = id
 
     this.sayhi = function () {
         return `Hola soy  ${this.name}`
     }
 
-    
+
     this.showStars = () => {
-        return `Nombre: ${this.name} , Altura: ${this.height} , Vida: ${this.hp} ,
-     Defensa: ${this.defense} , Ataque: ${this.attackPoints}`
+        return `Nombre: ${this.name} , Altura: ${this.height} , Vida: ${this.hp} , Defensa: ${this.defense} , Ataque: ${this.attackPoints}`
     }
 
 
-    //this.attack = (enemy) => {
-    //enemy.hp -= (this.attackPoints - enemy.defense)
-    //}
+    this.attack = (enemy) => {
+        enemy.hp -= (this.attackPoints - enemy.defense)
+    }
 
+
+    img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png`
 }
 
-const pikachu = new Pokemon(`Pikachu`, 4, 35, 40, 55)
-const charmander = new Pokemon(`Charmander`, 6, 39, 52, 43)
+const pikachu = new Pokemon(25 `Pikachu`, 4, 35, 40, 55)
+const charmander = new Pokemon(4 `Charmander`, 6, 39, 52, 43)
+
+
+pikachu.attack(charmander)
+charmander.attack(pikachu)
+
+
